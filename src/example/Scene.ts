9d0e4c421dcd251713b9d6fb3558c858024c6502
ntaso2051 <normalforce1227@gmail.com>
2020-09-06 04:57:24 +0900

    add Immediate Scene move

--- src/example/Scene.ts
+++ src/example/Scene.ts
@@ -1,25 +1,49 @@
 import * as PIXI from 'pixi.js';
-import UpdateObject from '../interfaces/UpdateObject'
+import UpdateObject from '../interfaces/UpdateObject';
+import Transition from '../interfaces/Transition';
+import Immediate from '../example/transition/Immediate';
 
 export default abstract class Scene extends PIXI.Container {
-    public update(delta: number): void {
-        this.updateRegisteredObjects(delta);
+  protected transitionIn: Transition = new Immediate();
+  protected transitionOut: Transition = new Immediate();
+
+  public update(delta: number): void {
+    if (this.transitionIn.isActive()) {
+      this.transitionIn.update(delta);
+    } else if (this.transitionOut.isActive()) {
+      this.transitionOut.update(delta);
+    }
+  }
+
+  protected registerUpdatingObject(object: UpdateObject): void {
+    console.log(object);
+  }
+
+  protected updateRegisteredObjects(delta: number): void {
+    console.log(delta);
+  }
+
+  public beginTransitionIn(onTransitionFinished: (scene: Scene) => void): void {
+    this.transitionIn.setCallback(() => onTransitionFinished(this));
+
+    const container = this.transitionIn.getContainer();
+    if (container) {
+      this.addChild(container);
     }
 
-    protected registerUpdatingObject(object: UpdateObject): void {
-        console.log(object);
+    this.transitionIn.begin();
+  }
+
+  public beginTransitionOut(
+    onTransitionFinished: (scene: Scene) => void
+  ): void {
+    this.transitionOut.setCallback(() => onTransitionFinished(this));
+
+    const container = this.transitionOut.getContainer();
+    if (container) {
+      this.addChild(container);
     }
 
-    protected updateRegisteredObjects(delta: number): void {
-        console.log(delta);
-    }
-
-    public beginTransitionIn(onTransitionFinished: (scene: Scene)=>void): void {
-        onTransitionFinished(this);
-    }
-
-    public beginTransitionOut(onTransitionFinished: (scene: Scene)=>void): void {
-        onTransitionFinished(this);
-    }
-
+    this.transitionOut.begin();
+  }
 }
