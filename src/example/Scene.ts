d0296aef9b392733c21f2217ab12dd43ff3c6953
ntaso2051 <normalforce1227@gmail.com>
2020-09-05 23:54:08 +0900

    add interfaces

---
+++ src/example/Scene.ts
@@ -0,0 +1,21 @@
+import * as PIXI from 'pixi.js';
+import UpdateObject from '../interfaces/UpdateObject'
+
+export default abstract class Scene extends PIXI.Container {
+    public update(delta: number): void {
+        this.updateRegisteredObjects(delta);
+    }
+
+    protected registerUpdatingObject(object: UpdateObject): void {
+        console.log(object);
+    }
+
+    protected updateRegisteredObjects(delta: number): void {
+        console.log(delta);
+    }
+
+    public beginTransitionIn(onTransitionFinished: (scene: Scene)=>void): void {
+        onTransitionFinished(this);
+    }
+
+}
